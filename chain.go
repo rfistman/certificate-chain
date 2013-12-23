b0ba2d1fe2b16c3009da75ffc834d72eeb5b2a9a
Gordon Childs <gordon.childs@gmail.com>
2013-12-23 14:18:06 +1100

    wip to der with proper signing

--- chain.go
+++ chain.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/rand"
 	"crypto/rsa"
+	"crypto/tls"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
 	"flag"
+	"io/ioutil"
 	"log"
 	"math/big"
 	"os"
@@ -22,9 +24,14 @@ var (
 	validFor     = flag.Duration("duration", 365*24*time.Hour, "Duration that certificate is valid for")
 	organization = flag.String("organization", "Async Pty. Ltd.", "organization - not the CN?")
 	isCA         = flag.Bool("ca", false, "whether this cert should be its own Certificate Authority")
+	certType     = flag.String("type", "ca", "ca|client|server")
+	dstDir       = flag.String("dst", ".", "destination directory for cert + key")
+	parentDir    = flag.String("parent", "", "parent certificate")
 )
 
 func main() {
+	flag.Parse()
+
 	priv, err := rsa.GenerateKey(rand.Reader, *rsaBits)
 
 	if err != nil {
@@ -55,28 +62,68 @@ func main() {
 		template.KeyUsage |= x509.KeyUsageCertSign
 	}
 
-	derBytes, err := x509.CreateCertificate(rand.Reader, &template, &template, &priv.PublicKey, priv)
+	parent := &template // default to self-signed
+
+	if len(*parentDir) > 0 {
+		if false {
+			//type Certificate
+			parentCert, err := tls.LoadX509KeyPair(*parentDir+"/cert.pem", *parentDir+"/key.pem")
+			if err != nil {
+				log.Fatalf("failed to open parent cert", err)
+			}
+			// is this a shortcut? tls.Cert->x509.Cert (pem->der) should probably just use der throughout
+			parent = parentCert.Leaf
+			log.Println("parent ", parent)
+		} else {
+			asn1Cert, err := ioutil.ReadFile(*parentDir + "/cert.cer")
+			if err != nil {
+				log.Fatal("failed to read parent cert", err)
+			}
+			parentCert, err := x509.ParseCertificate(asn1Cert)
+			parent = parentCert
+		}
+	}
+
+	switch *certType {
+	case "ca":
+	case "client":
+	case "server":
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &template, parent, &priv.PublicKey, priv)
 	if err != nil {
 		log.Fatalf("Failed to create certificate: %s", err)
 		return
 	}
 
-	certOut, err := os.Create("cert.pem")
-	if err != nil {
-		log.Fatalf("failed to open cert.pem for writing: %s", err)
-		return
-	}
-	pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	certOut.Close()
-	log.Print("written cert.pem\n")
+	if true {
+		err = ioutil.WriteFile(*dstDir+"/cert.cer", derBytes, 0600)
+		if err != nil {
+			log.Fatalf("failed to open cert.cer for writing: %s", err)
+		}
 
-	keyOut, err := os.OpenFile("key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Print("failed to open key.pem for writing:", err)
-		return
+		err = ioutil.WriteFile(*dstDir+"/key.der", x509.MarshalPKCS1PrivateKey(priv), 0600)
+		if err != nil {
+			log.Fatalf("failed to open key for writing: %s", err)
+		}
+	} else {
+		certOut, err := os.Create(*dstDir + "/cert.pem")
+		if err != nil {
+			log.Fatalf("failed to open cert.pem for writing: %s", err)
+			return
+		}
+		pem.Encode(certOut, &pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
+		certOut.Close()
+		log.Print("written cert.pem\n")
+
+		keyOut, err := os.OpenFile(*dstDir+"/key.pem", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0600)
+		if err != nil {
+			log.Print("failed to open key.pem for writing:", err)
+			return
+		}
+		pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
+		keyOut.Close()
+		log.Print("written key.pem\n")
 	}
-	pem.Encode(keyOut, &pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(priv)})
-	keyOut.Close()
-	log.Print("written key.pem\n")
 
 }
